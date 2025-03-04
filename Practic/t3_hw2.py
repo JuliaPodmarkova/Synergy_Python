0c77ca19fac14b2991a54f9a7d7f06472f770abb
Julia <hunter-777@yandex.ru>
2025-03-04 19:34:59 +0300

    +

--- Practic/t3_hw2.py
+++ Practic/t3_hw2.py
@@ -5,14 +5,7 @@ bad_count = 0
 
 while True:
     candy_bar = int(input('Введите вес батончика в граммах: '))
-    if candy_bar >= 40 and candy_bar <= 50:
-        count += 1
-        print('GOOD')
-        continue
-    elif candy_bar != 0 and candy_bar < 40 or candy_bar > 50:
-        bad_count += 1
-        print('BAD')
-        continue
+
     else:
         n = bad_count / (count + bad_count)
         print(f'{n:.2f}')
