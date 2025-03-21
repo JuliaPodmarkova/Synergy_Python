00757fcb557f9b07bad73259037cbe5dabd9c651
Julia <151451268+JuliaPodmarkova@users.noreply.github.com>
2025-03-21 15:12:49 +0300

    Update T10_API_Excampel.py

--- Practic/T10_API_Excampel.py
+++ Practic/T10_API_Excampel.py
@@ -3,7 +3,7 @@
 import requests
 timezone = 'Europe/Moscow'
 api_url = 'https://api.api-ninjas.com/v1/worldtime?timezone={}'.format(timezone)
-response = requests.get(api_url, headers={'X-Api-Key': 'I3b6IFmrQq0NS2mf9Pb5lQ==C7lbKwKhrFwwUu9k'})
+response = requests.get(api_url, headers={'X-Api-Key': 'I3b6IFmrQq0NS**********wKhrFwwUu9k'})
 if response.status_code == requests.codes.ok:
     print(response.text)
 else:
